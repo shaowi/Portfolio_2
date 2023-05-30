7c3ff3d685eaf6daf243bb63c03e6597c853bf95
Lee ShaoWee <lee_shao_wee@hotmail.com>
2023-05-30 20:49:35 +0800

    Updated icon to be clickable at whitespace

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,36 +5,28 @@ import { SiCodewars } from 'react-icons/si';
 const LINKEDIN_URL = 'https://www.linkedin.com/in/lee-shao-wee-18a381169/ ';
 const GITHUB_URL = 'https://github.com/shaowi';
 const CW_URL = 'https://www.codewars.com/users/shao4321';
+const urlToIconList = [
+  {
+    url: LINKEDIN_URL,
+    icon: <AiFillLinkedin />,
+  },
+  {
+    url: GITHUB_URL,
+    icon: <AiFillGithub />,
+  },
+  {
+    url: CW_URL,
+    icon: <SiCodewars />,
+  },
+];
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div>
-      <a
-        href={LINKEDIN_URL}
-        target="_blank
-        "
-      >
-        <AiFillLinkedin />
+    {urlToIconList.map(({ url, icon }) => (
+      <a href={url} target="_blank" rel="noreferrer">
+        <div>{icon}</div>
       </a>
-    </div>
-    <div>
-      <a
-        href={GITHUB_URL}
-        target="_blank
-        "
-      >
-        <AiFillGithub />
-      </a>
-    </div>
-    <div>
-      <a
-        href={CW_URL}
-        target="_blank
-        "
-      >
-        <SiCodewars />
-      </a>
-    </div>
+    ))}
   </div>
 );
 
